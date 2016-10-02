795d7595836834275eeff6501a1736c4a9e16b96
Joshua Vaughan <joshcv2112@gmail.com>
2016-10-02 13:58:26 -0600

    Completed floor cost problem

---
+++ floor_cost.py
@@ -0,0 +1,8 @@
+print 'Enter the cost per tile: '
+cost = raw_input()
+print 'Enter the height of tiles: '
+height = raw_input()
+print 'Enter the width of tiles: '
+width = raw_input()
+total_cost = int(height) * int(width) * int(cost)
+print 'Total cost: ', total_cost
