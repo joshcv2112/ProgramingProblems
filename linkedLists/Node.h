58e627e32df33f889a7c1c347b6c927bd3ffbaa7
Joshua Vaughan <joshcv2112@gmail.com>
2016-10-06 18:28:59 -0600

    Began linked list program.

---
+++ linkedLists/Node.h
@@ -0,0 +1,7 @@
+class Node
+{
+  Node(int);
+  
+  int value;
+  Node *next;	
+};
---
+++ linkedLists/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Node.h"
+using namespace std;
+
+int main()
+{
+  Node v1 = new Node();
+    Node v1.value = 10;
+    cout << v1.value << endl;
+    return 0;
+}
