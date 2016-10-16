2333f167bf8c8df952ab7f4bad32d0ef603d559e
Joshua Vaughan <joshcv2112@gmail.com>
2016-10-16 17:48:51 -0600

    first program for C++ Algorithms Udemy course

---
+++ AlgorithmsCPP/inputAndOutput.cpp
@@ -0,0 +1,21 @@
+/*
+A simple program demonstrating file input and output.
+*/
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ifstream f("data.in");
+ofstream g("data.out");
+  
+int main(){
+  int a, b, sum;
+
+  f >> a >> b;  
+  sum = a + b;
+  g << sum;
+
+  return 0;
+}
