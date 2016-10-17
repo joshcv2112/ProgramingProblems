25e0def5e6d2655f58dbe62fb382e1ece8e8996a
Joshua Vaughan <joshcv2112@gmail.com>
2016-10-16 21:51:03 -0600

    global/local variables example

---
+++ AlgorithmsCPP/STLExample.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 5, b = 8, maximum;
+
+    // maximum from a and b 
+    maximum = max(a, b);
+
+    // swapping two values
+    swap(a, b);
+
+    // double pow (double base, double exponent);
+
+    int number = 2;
+    double cubicRoot;
+
+    cubicRoot = pow((double)(number), 1.0/3);
+
+    cout << cubicRoot << endl;
+    cout << fixed << setprecision(10) << cubicRoot << "\n";
+    cout << fixed << setprecision(3) << cubicRoot << "\n";
+
+    return 0;
+}
---
+++ AlgorithmsCPP/globalLocalVariables.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int Aglobal[3];
+int a;
+
+void printing()
+{
+    cout << a << endl;
+}
+
+int main()
+{
+    int Alocal[3];
+
+    a = 5;
+    
+    printing();
+
+    return 0;
+}
