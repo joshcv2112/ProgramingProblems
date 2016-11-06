da10e2a24efd0c142c20fa9a65404dcfce1ec698
Joshua Vaughan <joshcv2112@gmail.com>
2016-11-06 17:20:44 +0000

    created mergeSort.cpp algorithm

--- IntroToAlgorithms/Sorting/insertionSort.cpp
+++ IntroToAlgorithms/Sorting/insertionSort.cpp
@@ -18,17 +18,15 @@ int main()
     }
     cout << endl;
 
-    int key, i;
-    for (int j = 2; j < SIZE; j++)
+    int i, j, key;
+    for (j = 1; j < SIZE; j++)
     {
         key = A[j];
-        i = j - 1;
-        while (i > 0 && A[i] > key)
+        for (i = j - 1; (i >= 0) && (A[i] > key); i--)
         {
-            A[i + 1] = A[i];
-            i = i - 1;
+            A[i+1] = A[i];
         }
-        A[i + 1] = key;
+        A[i+1] = key;
     }
 
     cout << "Array before sorting: ";
