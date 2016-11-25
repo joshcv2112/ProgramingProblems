4a26c0af5cdeafbf81a92e0ed423e81c5c694ddf
Joshua Vaughan <joshcv2112@gmail.com>
2016-11-24 23:18:11 -0700

    Completed section 2, lecture 2 of Advanced C++ course

---
+++ Advanced C++ Programming/Exceptions/basicExceptions.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+void potentialError();
+void usesPotentialError();
+
+// This additional function shows that throws can be caught
+// for any number of nested functions.
+void usesPotentialError()
+{
+    potentialError();
+}
+
+void potentialError()
+{
+    // Toggle these variables to test different errors,
+    // only one can be used (set to true) at once.
+    bool error1 = false;
+    bool error2 = false;
+    bool error3 = true;
+
+    // Test throw of type integer.
+    if (error1)
+    {
+        throw 8;
+    }
+
+    // Test throw primitive string type (char).
+    if (error2)
+    {
+        throw "An error has occured (primitive string)";
+    }
+
+    // Test throw of string object type.
+    if (error3)
+    {
+        throw string("An error has occured (string object)");
+    }
+}
+
+int main()
+{
+    try
+    {
+        usesPotentialError();
+    }
+    catch(int e)
+    {
+        cout << "Error #" << e << endl;
+    }
+    catch(char const * e)
+    {
+        cout << e << endl;
+    }
+    catch(string &e)
+    {
+        cout << e << endl;
+    }
+    return 0;
+}
