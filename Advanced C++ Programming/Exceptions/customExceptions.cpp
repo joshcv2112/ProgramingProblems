61848b0c27a9055d66c82bdaa805c94d7a098978
Joshua Vaughan <joshcv2112@gmail.com>
2016-11-25 22:50:30 -0700

    Completed Lesson 4 Advanced C++ Course

---
+++ Advanced C++ Programming/Exceptions/customExceptions.cpp
@@ -0,0 +1,43 @@
+/*
+Author: Joshua Vaughan
+Description: Lesson 2: Custom Exceptions, Advanced C++ Course
+Demonstrates how custom exceptions can be used using the exception class
+*/
+
+#include <iostream>
+#include <exception>
+
+using namespace std;    
+
+class MyException: public exception 
+{
+public:
+    virtual const char* what() const throw()
+    {
+        return "Something bad hapened!";
+    }
+};
+
+class Test
+{
+public:
+    void goesWrong()
+    {
+        throw MyException();
+    }
+};
+
+int main()
+{
+    Test test;
+    try 
+    {
+        test.goesWrong();
+    }
+    catch(MyException &e)
+    {
+        cout << e.what() << endl;
+    }
+
+    return 0;
+}
